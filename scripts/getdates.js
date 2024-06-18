3300a4ae1a2b439190485f49d3e6e327d8ab4f1a
ttrampp <ttrampp@msn.com>
2024-06-17 23:30:44 -0600

    getdates

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,5 +1,5 @@
 
 <script>
-let text = document.lastModified;
-document.getElementById("date-modified").innerHTML = text;
+    let text = document.lastModified;
+    document.getElementById("date-modified").innerHTML = text;
 </script>
