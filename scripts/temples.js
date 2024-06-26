16d17e42c53bac1ea3a22dae04d50a81ebd3c792
ttrampp <ttrampp@msn.com>
2024-06-26 00:55:01 -0600

    temples.css large

---
+++ scripts/temples.js
@@ -0,0 +1,5 @@
+let text = document.lastModified;
+document.getElementById("date-modified").innerHTML = text;
+
+const hamburger = document.querySelector('.hamburger');
+const pages = document.querySelector('.pages');
