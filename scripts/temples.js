17b951a2e6272f883e1a517e589f0a1efbdea098
ttrampp <ttrampp@msn.com>
2024-06-26 16:45:54 -0600

    temples.js

--- scripts/temples.js
+++ scripts/temples.js
@@ -3,3 +3,11 @@ document.getElementById("date-modified").innerHTML = text;
 
 const hamburger = document.querySelector('show');
 const pages = document.querySelector('show');
+
+hamburger.addEventListener('click', () => {
+    pages.classList.toggle('open');
+    title.style.display = pages.classList.contains('open') ? 'none' : 'block';
+    hamburger.textContent = pages.classList.contains('open') ? '❌' : '☰';
+});
+
+console.log("test");
