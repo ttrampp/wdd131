1ea63922fff6171933b98c69e975dc1a9db66087
ttrampp <ttrampp@msn.com>
2024-07-01 21:01:22 -0600

    windchill

--- week03/scripts/place.js
+++ week03/scripts/place.js
@@ -1,4 +1,13 @@
+// last date modified
 <script>
     let text = document.lastModified;
     document.getElementById("date-modified").innerHTML = text;
 </script>
+
+// windchill factor
+var temp= 92;
+var wSpeed= 13;
+var windChill= (35.74 + (0.6215 * temp))-(35.75 * Math.pow(wSpeed,0.16)) + (0.4275*temp*Math.pow(wSpeed,0.16));
+
+var windChill= Math.round(windChill);
+document.getElementById("windChill").innerHTML= windChill;
