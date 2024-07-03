ab32f010bb526f0eab438339a48de9acefb8adec
ttrampp <ttrampp@msn.com>
2024-07-02 22:18:11 -0600

    place html and css and large css

--- week03/scripts/place.js
+++ week03/scripts/place.js
@@ -5,9 +5,17 @@
 </script>
 
 // windchill factor
-var temp= 92;
-var wSpeed= 13;
-var windChill= (35.74 + (0.6215 * temp))-(35.75 * Math.pow(wSpeed,0.16)) + (0.4275*temp*Math.pow(wSpeed,0.16));
 
-var windChill= Math.round(windChill);
+function calculateWindChill(temp, wSpeed) {
+    var temp= 92;
+    var wSpeed= 13;
+    if (temp <=50 && wSpeed >=3){
+        var windChill= (35.74 + (0.6215 * temp))-(35.75 * Math.pow(wSpeed,0.16)) + (0.4275*temp*Math.pow(wSpeed,0.16));
+        var windChill= Math.round(windChill);
+        return windChill}
+    else 
+        {return 'N/A'}
+}
+
+var windChill = calculateWindChill(92, 13); 
 document.getElementById("windChill").innerHTML= windChill;
