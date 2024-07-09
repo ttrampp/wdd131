a01cf37782564ebf4c6a0978a369f5cd037beac2
ttrampp <ttrampp@msn.com>
2024-07-08 22:35:07 -0600

    windchill

--- scripts/place.js
+++ scripts/place.js
@@ -6,17 +6,35 @@
 
 // windchill factor
 
+
+
+const temp = 92;
+const wSpeed = 13;
+
 function calculateWindChill(temp, wSpeed) {
-    var temp= 92;
-    var wSpeed= 13;
-    if (temp <=50 && wSpeed >=3){
-        var windChill= (35.74 + (0.6215 * temp))-(35.75 * Math.pow(wSpeed,0.16)) + (0.4275*temp*Math.pow(wSpeed,0.16));
-        var windChill= Math.round(windChill);
-        return windChill}
-    else 
-        {return 'N/A'}
+    return 13.12 + (0.6215 * temp) - (11.37 * Math.pow(wSpeed, 0.16)) + (0.3965 * temp * Math.pow(wSpeed, 0.16));
 }
 
 
-var windChill = calculateWindChill(92, 13); 
-document.getElementById("windChill").innerHTML= windChill;
+function displayWindChill(temp, wSpeed) {
+    var temp= 92;
+    var wSpeed= 13;
+    if (temp <=50 && wSpeed >=3){
+      //  var windChill= (35.74 + (0.6215 * temp))-(35.75 * Math.pow(wSpeed,0.16)) + (0.4275*temp*Math.pow(wSpeed,0.16));
+        //var windChill= Math.round(windChill);
+        //return windChill}
+        windChill = calculateWindChill(temp, wSpeed);
+    }
+    document.getElementById("windChill").innerHTML = windChill !== "N/A" ? "${WindChill}" : windChill;
+}
+    //else 
+//        {return 'N/A'}
+//}
+
+
+//var windChill = calculateWindChill(92, 13); 
+//document.getElementById("windChill").innerHTML= windChill;
+
+
+
+displayWindChill(temp, wSpeed);
