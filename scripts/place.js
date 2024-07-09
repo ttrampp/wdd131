d789c5a8458e5dc8ef755a00bfd657bf8210d702
ttrampp <ttrampp@msn.com>
2024-07-09 07:31:10 -0600

    windchill

--- scripts/place.js
+++ scripts/place.js
@@ -25,19 +25,21 @@ function getLastModified () {
 //}
 
 
-//function displayWindChill(temp, wSpeed) {
-  //  var temp= 92;
-    //var wSpeed= 13;
-    //if (temp <=50 && wSpeed >=3){
-      //  var windChill= (35.74 + (0.6215 * temp))-(35.75 * Math.pow(wSpeed,0.16)) + (0.4275*temp*Math.pow(wSpeed,0.16));
-        //var windChill= Math.round(windChill);
-        //return windChill}
-      //  windChill = calculateWindChill(temp, wSpeed);
-    //}
-    //document.getElementById("windChill").innerHTML = windChill !== "N/A" ? "${WindChill}" : windChill;
-//}
-    //else 
-//        {return 'N/A'}
+function displayWindChill(temp, wSpeed) {
+    var temp= 92;
+    var wSpeed= 13;
+    if (temp <=50 && wSpeed >=3){
+        var windChill= (35.74 + (0.6215 * temp))-(35.75 * Math.pow(wSpeed,0.16)) + (0.4275*temp*Math.pow(wSpeed,0.16));
+        var windChill= Math.round(windChill);
+        return windChill}
+        windChill = calculateWindChill(temp, wSpeed);
+    else
+      {return 'N/A'}
+    }
+    document.getElementById("windChill").innerHTML = windChill !== "N/A" ? "${WindChill}" : windChill;
+
+    //else
+      //{return 'N/A'}
 //}
 
 
